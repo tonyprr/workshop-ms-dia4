b2543aa4734cc8240cd70165c9dd504229045523
illarisys <miguel_luyo86@hotmail.com>
2016-08-13 11:04:11 -0500

    primer commit

---
+++ src/main/java/com/cacti/workshop/Dia4Application.java
@@ -0,0 +1,12 @@
+package com.cacti.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dia4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dia4Application.class, args);
+	}
+}
