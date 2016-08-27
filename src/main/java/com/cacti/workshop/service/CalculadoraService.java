206cf11e91398313fa4c781d5b63709a3f9a2a18
tonyprr <tonyprr@gmail.com>
2016-08-27 07:53:57 -0500

    se agrega funciones de suma y resta.

---
+++ src/main/java/com/cacti/workshop/service/CalculadoraService.java
@@ -0,0 +1,19 @@
+package com.cacti.workshop.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by desarrollo on 27/08/16.
+ */
+@Service
+public class CalculadoraService {
+
+    public  Integer sum(Integer ope1, Integer ope2) {
+        return ope1 + ope2;
+    }
+
+    public  Integer subs(Integer ope1, Integer ope2) {
+        return ope2 - ope1;
+    }
+
+}
