cefd2d88639feaef4d601c17be825dd197c3256a
tonyprr <tonyprr@gmail.com>
2016-08-27 09:06:00 -0500

    test de suma

---
+++ src/test/java/com/cacti/workshop/service/CalculadoraServiceTests.java
@@ -0,0 +1,17 @@
+package com.cacti.workshop.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by desarrollo on 27/08/16.
+ */
+public class CalculadoraServiceTests {
+    @Test
+    public  void test_sum() {
+        CalculadoraService service = new CalculadoraService();
+        assertEquals("La suma de 1 y 2 tiene que ser 3", Integer.valueOf(3), service.sum(1, 2));
+    }
+}
