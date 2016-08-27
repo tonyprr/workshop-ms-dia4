78d51d86d9fe193fd9be41561f1c0638d5032cd9
tonyprr <tonyprr@gmail.com>
2016-08-27 15:47:03 -0500

    se agrego test de resta.

--- src/test/java/com/cacti/workshop/service/CalculadoraServiceTests.java
+++ src/test/java/com/cacti/workshop/service/CalculadoraServiceTests.java
@@ -14,4 +14,11 @@ public class CalculadoraServiceTests {
         CalculadoraService service = new CalculadoraService();
         assertEquals("La suma de 1 y 2 tiene que ser 3", Integer.valueOf(3), service.sum(1, 2));
     }
+
+    @Test
+    public  void test_subs() {
+        CalculadoraService service = new CalculadoraService();
+        assertEquals("La resta de 2 y 1tiene que ser 1", Integer.valueOf(1), service.subs(2, 1));
+    }
+
 }
